42b8d8797cbda905bbfff3664e4ff1468e828340
Kilian Cantel <laptoff.contact@gmail.com>
2024-12-15 19:16:31 +0100

    Dockerize project (0.1).

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const path = "/home/kilian/Documents/Projects/To-Do List";
+const path = __dirname;
 
 app.get('/home', (req, res) => {
     res.sendFile(path + '/Front/index.html');
